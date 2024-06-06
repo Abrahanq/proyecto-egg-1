0fc6acc8cb2fefa365eb1dc493dee0d9a5f17444
Ignacio Gabriel Flores <chinitochitoo@gmail.com>
2024-06-05 23:00:18 -0300

    entidades

--- src/main/java/com/servicios/egg/entidades/Provedor.java
+++ src/main/java/com/servicios/egg/entidades/Provedor.java
@@ -2,6 +2,7 @@ package com.servicios.egg.entidades;
 
 import java.util.List;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -9,7 +10,10 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import lombok.Data;
 
+@Data
+@Entity
 public class Provedor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
